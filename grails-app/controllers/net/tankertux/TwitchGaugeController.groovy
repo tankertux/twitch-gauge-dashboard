9dd512ede0610da8c72f1035e469e644738537ba
Jordan Grant <tankertux@gmail.com>
2015-06-13 16:36:32 -0500

    Made TwitchGauge service and controller. Renamed from GaugeService due to namespace collision issues.

---
+++ grails-app/controllers/net/tankertux/TwitchGaugeController.groovy
@@ -0,0 +1,12 @@
+package net.tankertux
+
+import net.tankertux.TwitchGaugeService
+
+class TwitchGaugeController {
+
+	TwitchGaugeService twitchGaugeService
+	
+    def index() { 
+		return [gauges: twitchGaugeService.listAll()]
+	}
+}
---
+++ grails-app/services/net/tankertux/TwitchGaugeService.groovy
@@ -0,0 +1,12 @@
+package net.tankertux
+
+import grails.transaction.Transactional
+
+@Transactional
+class TwitchGaugeService {
+
+	@Transactional(readOnly = true)
+    def listAll() {
+		return Gauge.findAll()
+    }
+}
