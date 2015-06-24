e48e5a4d7760a72d94244ab37ad6c1770b9b5ae9
Jordan Grant <tankertux@gmail.com>
2015-06-24 06:47:01 -0500

    Added domain constraints. Still need view validations.
    Added convienience readonly "field" webSafeLabel

--- grails-app/domain/net/tankertux/TwitchGauge.groovy
+++ grails-app/domain/net/tankertux/TwitchGauge.groovy
@@ -10,5 +10,14 @@ class TwitchGauge {
 	Integer invocations = 0;
 
     static constraints = {
+		label blank: false, unique: true
+		keyphrase blank: false
+		capacity blank: false
     }
+	
+	public String getWebSafeLabel(){
+		return label.replaceAll('\\s+', '')
+	}
+	
+	static transients = ['webSafeLabel']
 }
