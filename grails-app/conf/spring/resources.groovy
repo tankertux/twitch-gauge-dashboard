7e8b211cafbe3245d8e4c18685404305a94334d4
Jordan Grant <tankertux@gmail.com>
2015-10-02 16:39:20 -0500

    Wired up beans for TwitchBot to the TwitchGaugeService

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,16 @@
 // Place your Spring DSL code here
+import net.tankertux.TwitchBot
+
 beans = {
+    bot(TwitchBot)  { bean ->
+        twitchGaugeService = ref("twitchGaugeService")
+        nickname = 'TankertuxBot'
+    }
+
+    host String, 'irc.twitch.tv'
+    port Integer, 6667
+    //TankertuxBot
+    password String, "REDACTED"
+
+    channel String, '#tankertux'
 }
